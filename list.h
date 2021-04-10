b377a7f0acd4660ca7888872a75688386303f941
jhan1998 <jhan1998@gmail.com>
2021-04-10 21:47:00 +0800

    Add standalone code of list.h`

---
+++ list.h
@@ -0,0 +1,341 @@
+#ifndef _LINUX_LIST_H
+#define _LINUX_LIST_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <time.h>
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#define true 1
+#define false 0
+
+/**
+ * container_of() - Calculate address of object that contains address ptr
+ * @ptr: pointer to member variable
+ * @type: type of the structure containing ptr
+ * @member: name of the member variable in struct @type
+ *
+ * Return: @type pointer of object containing ptr
+ */
+#ifndef container_of
+#define container_of(ptr, type, member)                            \
+    __extension__({                                                \
+        const __typeof__(((type *) 0)->member) *__pmember = (ptr); \
+        (type *) ((char *) __pmember - offsetof(type, member));    \
+    })
+#endif
+
+/**
+ * struct list_head - Head and node of a doubly-linked list
+ * @prev: pointer to the previous node in the list
+ * @next: pointer to the next node in the list
+ */
+struct list_head {
+    struct list_head *prev, *next;
+};
+
+struct listitem {
+    uint16_t i;
+    struct list_head list;
+};
+
+/**
+ * LIST_HEAD - Declare list head and initialize it
+ * @head: name of the new object
+ */
+#define LIST_HEAD(head) struct list_head head = {&(head), &(head)}
+
+/**
+ * INIT_LIST_HEAD() - Initialize empty list head
+ * @head: pointer to list head
+ */
+static inline void INIT_LIST_HEAD(struct list_head *head)
+{
+    head->next = head; head->prev = head;
+}
+
+/**
+ * list_add() - Add a list node to the beginning of the list
+ * @node: pointer to the new node
+ * @head: pointer to the head of the list
+ */
+static inline void list_add(struct list_head *node, struct list_head *head)
+{
+    struct list_head *next = head->next;
+
+    next->prev = node;
+    node->next = next;
+    node->prev = head;
+    head->next = node;
+}
+
+/**
+ * list_add_tail() - Add a list node to the end of the list
+ * @node: pointer to the new node
+ * @head: pointer to the head of the list
+ */
+static inline void list_add_tail(struct list_head *node, struct list_head *head)
+{
+    struct list_head *prev = head->prev;
+
+    prev->next = node;
+    node->next = head;
+    node->prev = prev;
+    head->prev = node;
+}
+
+/**
+ * list_del() - Remove a list node from the list
+ * @node: pointer to the node
+ */
+static inline void list_del(struct list_head *node)
+{
+    struct list_head *next = node->next, *prev = node->prev;
+    next->prev = prev; prev->next = next;
+}
+
+/**
+ * list_move() - Move a list node to the beginning of the list
+ * @node: pointer to the node
+ * @head: pointer to the head of the list
+ *
+ * The @node is removed from its old position/node and add to the beginning of
+ * @head
+ */
+static inline void list_move(struct list_head *node, struct list_head *head)
+{
+    list_del(node);
+    list_add(node, head);
+}
+
+/**
+ * list_move_tail() - Move a list node to the end of the list
+ * @node: pointer to the node
+ * @head: pointer to the head of the list
+ *
+ * The @node is removed from its old position/node and add to the end of @head
+ */
+static inline void list_move_tail(struct list_head *node,
+                                  struct list_head *head)
+{
+    list_del(node);
+    list_add_tail(node, head);
+}
+
+/**
+ * list_empty() - Check if list head has no nodes attached
+ * @head: pointer to the head of the list
+ */
+static inline int list_empty(const struct list_head *head)
+{
+    return (head->next == head);
+}
+
+/**
+ * list_is_singular() - Check if list head has exactly one node attached
+ * @head: pointer to the head of the list
+ */
+static inline int list_is_singular(const struct list_head *head)
+{
+    return (!list_empty(head) && head->prev == head->next);
+}
+
+/**
+ * list_splice() - Add list nodes from a list to beginning of another list
+ * @list: pointer to the head of the list with the node entries
+ * @head: pointer to the head of the list
+ *
+ * All nodes from @list are added to to the beginning of the list of @head.
+ * It is similar to list_add but for multiple nodes. The @list head is not
+ * modified and has to be initialized to be used as a valid list head/node
+ * again.
+ */
+static inline void list_splice(struct list_head *list, struct list_head *head)
+{
+    struct list_head *head_first = head->next;
+    struct list_head *list_first = list->next;
+    struct list_head *list_last = list->prev;
+
+    if (list_empty(list))
+        return;
+
+    head->next = list_first;
+    list_first->prev = head;
+
+    list_last->next = head_first;
+    head_first->prev = list_last;
+}
+
+/**
+ * list_splice_tail() - Add list nodes from a list to end of another list
+ * @list: pointer to the head of the list with the node entries
+ * @head: pointer to the head of the list
+ */
+static inline void list_splice_tail(struct list_head *list,
+                                    struct list_head *head)
+{
+    struct list_head *head_last = head->prev;
+    struct list_head *list_first = list->next, *list_last = list->prev;
+
+    if (list_empty(list))
+        return;
+
+    head->prev = list_last;
+    list_last->next = head;
+
+    list_first->prev = head_last;
+    head_last->next = list_first;
+}
+
+/**
+ * list_splice_init() - Move list nodes from a list to beginning of another list
+ * @list: pointer to the head of the list with the node entries
+ * @head: pointer to the head of the list
+ *
+ * All nodes from @list are added to to the beginning of the list of @head.
+ * It is similar to list_add but for multiple nodes.
+ *
+ * The @list head will not end up in an uninitialized state like when using
+ * list_splice. Instead the @list is initialized again to the an empty
+ * list/unlinked state.
+ */
+static inline void list_splice_init(struct list_head *list,
+                                    struct list_head *head)
+{
+    list_splice(list, head);
+    INIT_LIST_HEAD(list);
+}
+
+/**
+ * list_cut_position() - Move beginning of a list to another list
+ * @head_to: pointer to the head of the list which receives nodes
+ * @head_from: pointer to the head of the list
+ * @node: pointer to the node in which defines the cutting point
+ */
+static inline void list_cut_position(struct list_head *head_to,
+                                     struct list_head *head_from,
+                                     struct list_head *node)
+{
+    struct list_head *head_from_first = head_from->next;
+
+    if (list_empty(head_from))
+        return;
+
+    if (head_from == node) {
+        INIT_LIST_HEAD(head_to);
+        return;
+    }
+
+    head_from->next = node->next;
+    head_from->next->prev = head_from;
+
+    head_to->prev = node;
+    node->next = head_to;
+    head_to->next = head_from_first;
+    head_to->next->prev = head_to;
+}
+
+/**
+ * list_entry() - Calculate address of entry that contains list node
+ * @node: pointer to list node
+ * @type: type of the entry containing the list node
+ * @member: name of the list_head member variable in struct @type
+ */
+#define list_entry(node, type, member) container_of(node, type, member)
+
+/**
+ * list_first_entry() - get first entry of the list
+ * @head: pointer to the head of the list
+ * @type: type of the entry containing the list node
+ * @member: name of the list_head member variable in struct @type
+ */
+#define list_first_entry(head, type, member) \
+    list_entry((head)->next, type, member)
+
+/**
+ * list_for_each - iterate over list nodes
+ * @node: list_head pointer used as iterator
+ * @head: pointer to the head of the list
+ */
+#define list_for_each(node, head) \
+    for (node = (head)->next; node != (head); node = node->next)
+
+/**
+ * list_for_each_entry_safe - iterate over list entries and allow deletes
+ * @entry: pointer used as iterator
+ * @safe: @type pointer used to store info for next entry in list
+ * @head: pointer to the head of the list
+ * @member: name of the list_head member variable in struct type of @entry
+ *
+ * The current node (iterator) is allowed to be removed from the list. Any
+ * other modifications to the the list will cause undefined behavior.
+ *
+ * FIXME: remove dependency of __typeof__ extension
+ */
+#define list_for_each_entry_safe(entry, safe, head, member)                \
+    for (entry = list_entry((head)->next, __typeof__(*entry), member),     \
+        safe = list_entry(entry->member.next, __typeof__(*entry), member); \
+         &entry->member != (head); entry = safe,                           \
+        safe = list_entry(safe->member.next, __typeof__(*entry), member))
+
+static inline uint8_t getnum(void)
+{
+    static uint16_t s1 = UINT16_C(2);
+    static uint16_t s2 = UINT16_C(1);
+    static uint16_t s3 = UINT16_C(1);
+
+    s1 *= UINT16_C(171);
+    s1 %= UINT16_C(30269);
+
+    s2 *= UINT16_C(172);
+    s2 %= UINT16_C(30307);
+
+    s3 *= UINT16_C(170);
+    s3 %= UINT16_C(30323);
+
+    return s1 ^ s2 ^ s3;
+}
+
+static uint16_t get_unsigned16(void)
+{
+    uint16_t x = 0;
+    size_t i;
+
+    for (i = 0; i < sizeof(x); i++) {
+        x <<= 8;
+        x |= getnum();
+    }
+
+    return x;
+}
+
+static inline int cmpint(const void *p1, const void *p2)
+{
+    const uint16_t *i1 = (const uint16_t *) p1;
+    const uint16_t *i2 = (const uint16_t *) p2;
+
+    return *i1 - *i2;
+}
+
+static inline int cmplist(void *arrange, struct list_head *a, struct list_head *b) {
+  const uint16_t ai = container_of(a, struct listitem, list)->i;
+  const uint16_t bi = container_of(b, struct listitem, list)->i;
+
+    return ai - bi;
+}
+
+static inline void random_shuffle_array(uint16_t *operations, uint16_t len)
+{
+    uint16_t i;
+    uint16_t j;
+
+    for (i = 0; i < len; i++) {
+        /* WARNING biased shuffling */
+        j = get_unsigned16() % (i + 1);
+        operations[i] = operations[j];
+        operations[j] = i;
+    }
+}
+#endif
